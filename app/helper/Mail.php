285fd3ea96b8b3b940b461c47199d4f431aecf0d
Loan Kim Nguyen <loan.nguyen2311883@hcmut.edu.vn>
2026-04-14 03:04:19 +0700

    Thêm smtp để gửi otp + session timeout

---
+++ app/helper/Mail.php
@@ -0,0 +1,29 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class Mail {
+    public static function sendOTP($email, $otp) {
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();                                            
+            $mail->Host       = $_ENV['MAIL_HOST'];                    
+            $mail->SMTPAuth   = true;                                   
+            $mail->Username   = $_ENV['MAIL_USER'];                     
+            $mail->Password   = $_ENV['MAIL_PASS'];                               
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         
+            $mail->Port       = $_ENV['MAIL_PORT'];                    
+
+            $mail->setFrom($_ENV['MAIL_USER'], $_ENV['SITENAME']);
+            $mail->addAddress($email);     
+
+            $mail->Subject = 'Mã xác nhận OTP - ' . $_ENV['SITENAME'];
+            $mail->Body    = "Mã OTP của bạn là: $otp. Mã có hiệu lực trong 5 phút.";
+            $mail->send();
+            return true;
+        } catch (Exception $e) {
+            error_log('PHPMailer error: ' . $mail->ErrorInfo);
+            return false;
+        }
+    }
+}
